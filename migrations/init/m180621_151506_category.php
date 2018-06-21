f324078a968811a4a4139ed4ef5c79bd331bd90a
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-06-21 18:33:04 +0200

    init 4

--- migrations/init/m180621_151506_category.php
+++ migrations/init/m180621_151506_category.php
@@ -8,7 +8,7 @@ class m180621_151506_category extends Migration
 
     public function init()
     {
-        $this->db = 'db';
+        $this->db = 'db'; 
         parent::init();
     }
 
@@ -26,19 +26,19 @@ class m180621_151506_category extends Migration
                 'lvl'=> $this->smallInteger(5)->notNull()->comment('Nested set level / depth'),
                 'name'=> $this->string(60)->notNull()->comment('The tree node name / label'),
                 'icon'=> $this->string(255)->null()->defaultValue(null)->comment('The icon to use for the node'),
-                'icon_type'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Icon Type: 1 = CSS Class, 2 = Raw Markup'),
-                'active'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is active (will be set to false on deletion)'),
-                'selected'=> $this->tinyint(1)->notNull()->defaultValue(0)->comment('Whether the node is selected/checked by default'),
-                'disabled'=> $this->tinyint(1)->notNull()->defaultValue(0)->comment('Whether the node is enabled'),
-                'readonly'=> $this->tinyint(1)->notNull()->defaultValue(0)->comment('Whether the node is read only (unlike disabled - will allow toolbar actions)'),
-                'visible'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is visible'),
-                'collapsed'=> $this->tinyint(1)->notNull()->defaultValue(0)->comment('Whether the node is collapsed by default'),
-                'movable_u'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is movable one position up'),
-                'movable_d'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is movable one position down'),
-                'movable_l'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is movable to the left (from sibling to parent)'),
-                'movable_r'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is movable to the right (from sibling to child)'),
-                'removable'=> $this->tinyint(1)->notNull()->defaultValue(1)->comment('Whether the node is removable (any children below will be moved as siblings before deletion)'),
-                'removable_all'=> $this->tinyint(1)->notNull()->defaultValue(0)->comment('Whether the node is removable along with descendants'),
+                'icon_type'=> $this->boolean()->notNull()->defaultValue(true)->comment('Icon Type: 1 = CSS Class, 2 = Raw Markup'),
+                'active'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is active (will be set to false on deletion)'),
+                'selected'=> $this->boolean()->notNull()->defaultValue(false)->comment('Whether the node is selected/checked by default'),
+                'disabled'=> $this->boolean()->notNull()->defaultValue(false)->comment('Whether the node is enabled'),
+                'readonly'=> $this->boolean()->notNull()->defaultValue(false)->comment('Whether the node is read only (unlike disabled - will allow toolbar actions)'),
+                'visible'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is visible'),
+                'collapsed'=> $this->boolean()->notNull()->defaultValue(false)->comment('Whether the node is collapsed by default'),
+                'movable_u'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is movable one position up'),
+                'movable_d'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is movable one position down'),
+                'movable_l'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is movable to the left (from sibling to parent)'),
+                'movable_r'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is movable to the right (from sibling to child)'),
+                'removable'=> $this->boolean()->notNull()->defaultValue(true)->comment('Whether the node is removable (any children below will be moved as siblings before deletion)'),
+                'removable_all'=> $this->boolean()->notNull()->defaultValue(false)->comment('Whether the node is removable along with descendants'),
                 'created_at'=> $this->integer(11)->notNull(),
                 'updated_at'=> $this->integer(11)->notNull(),
             ],$tableOptions
@@ -48,7 +48,6 @@ class m180621_151506_category extends Migration
         $this->createIndex('tbl_tree_NK3','{{%category}}',['rgt'],false);
         $this->createIndex('tbl_tree_NK4','{{%category}}',['lvl'],false);
         $this->createIndex('tbl_tree_NK5','{{%category}}',['active'],false);
-
     }
 
     public function safeDown()
