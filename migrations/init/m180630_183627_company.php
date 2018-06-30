4fed5a66cb127da069d6a90dea27b902a50f0873
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-06-30 17:55:10 +0200

    migration

--- migrations/init/m180630_183627_company.php
+++ migrations/init/m180630_183627_company.php
@@ -6,18 +6,7 @@ use yii\db\Migration;
 class m180630_183627_company extends Migration
 {
     public $tableName = '{{%company}}';
-    public $columns = [
-        'zip_code' => Schema::TYPE_STRING ."(6) NULL DEFAULT NULL",
-        'city' =>  Schema::TYPE_STRING ."(100) NULL DEFAULT NULL",
-        'street' =>  Schema::TYPE_STRING ."(100) NULL DEFAULT NULL",
-        'building'=> Schema::TYPE_STRING ."(5) NULL DEFAULT NULL",
-        'local'=> Schema::TYPE_STRING ."(5) NULL DEFAULT NULL",
-        'notes'=> Schema::TYPE_STRING ."(2000) NULL DEFAULT NULL",
-        'created_by'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
-        'created_at'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
-        'updated_by'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
-        'updated_at'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
-    ];
+    public $columns = [];
 
     public function init()
     {
@@ -27,6 +16,19 @@ class m180630_183627_company extends Migration
 
     public function safeUp()
     {
+        $this->columns = [
+            'zip_code' => Schema::TYPE_STRING ."(6) NULL DEFAULT NULL",
+            'city' =>  Schema::TYPE_STRING ."(100) NULL DEFAULT NULL",
+            'street' =>  Schema::TYPE_STRING ."(100) NULL DEFAULT NULL",
+            'building'=> Schema::TYPE_STRING ."(5) NULL DEFAULT NULL",
+            'local'=> Schema::TYPE_STRING ."(5) NULL DEFAULT NULL",
+            'notes'=> Schema::TYPE_STRING ."(2000) NULL DEFAULT NULL",
+            'created_by'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
+            'created_at'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
+            'updated_by'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
+            'updated_at'=> Schema::TYPE_INTEGER ."(11) NULL DEFAULT NULL",
+        ];
+
         foreach ($this->columns as $key => $value) {
             if(is_null($this->getDb()->getSchema()->getTableSchema($this->tableName)->getColumn($key ))) {
                 $this->addColumn(
