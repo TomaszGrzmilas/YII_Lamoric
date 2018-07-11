623537c1545403fa39a78a312dbed7885ea45a01
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-07-11 21:00:14 +0200

    schedule

--- tasks/Schedule.php
+++ tasks/Schedule.php
@@ -1,9 +1,18 @@
 <?php
 
 use \omnilight\scheduling\Schedule;
+use app\models\balance_account\BalanceAccount;
 
 $schedule = new Schedule();
 
 $schedule->call(function() {
-  echo "task";
-})->dailyAt('21:00');
+    $account = new BalanceAccount();
+    $account->balance = 100;
+    if ($account->save())
+    {
+
+    } else {
+        throw new UserException(Yii::t('app','Error when creating user account. Try again.'));
+    }
+
+})->dailyAt('21:10');
