4ed1ddee7e487f529c24de1dd81bcb0b6c0fa974
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-07-11 20:54:36 +0200

    shedule

--- tasks/Schedule.php
+++ tasks/Schedule.php
@@ -2,12 +2,8 @@
 
 use \omnilight\scheduling\Schedule;
 
-
 $schedule = new Schedule();
 
-$schedule->exec('ls')->everyFiveMinutes();
-
-// This command will call callback function every day at 10:00
-$schedule->call(function(\yii\console\Application $app) {
+$schedule->call(function() {
   echo "task";
 })->dailyAt('21:00');
