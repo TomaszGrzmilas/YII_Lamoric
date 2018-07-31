39c11b1e6c7aa85437ad92393772bfba1edbeeff
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-07-31 19:29:16 +0200

    fix categiries admin

--- views/category/admin.php
+++ views/category/admin.php
@@ -8,37 +8,39 @@ $item = $this->context->module->id . '-' . $this->context->id . '-' . $this->con
 $this->title = Yii::t('db/category', 'Categories');
 $this->params['breadcrumbs'][] = $this->title;
 
-echo TreeView::widget([
-    'query'             => $model::find()->addOrderBy('root, lft'), 
-    'headingOptions'    => ['label' => Yii::t('db/category', 'Categories')],
-    'fontAwesome' => true,     
-    'isAdmin'           => Yii::$app->user->can('Application Admin'),                       // optional (toggle to enable admin mode)
-    'showFormButtons' => true,
-    'iconEditSettings'=> [
-        'show' => 'list',
-        'type' => TreeView::ICON_CSS,
-        'listData' => [
-            'folder' => 'Folder',
-            'file' => 'File',
-            'mobile' => 'Phone',
-            'bell' => 'Bell',
-            'comments' => 'kropki',
-            'gavel' => 'mlotek',
-            'briefcase'=>'walizka',
-            'male' => 'facet',
-            'user-secret' => 'agent',
-            'balance-scale' => 'szalka',
-            'book'=>'książka',
-            'newspaper-o'=>'gazeta'
-        ]
-    ],
-//    'wrapperTemplate' =>'
-  //                      {header}
-  //                      {tree}',
-  //  'toolbarOptions'=> ['class'=>'hide'],
-    'displayValue'      => 1,                           // initial display value
-    'softDelete'      => true,                        // normally not needed to change
-    //'cacheSettings'   => ['enableCache' => true]      // normally not needed to change
-]);
-
+    if (Yii::$app->user->can('Application Admin'))
+    {
+        echo TreeView::widget([
+            'query'             => $model::find()->addOrderBy('root, lft'), 
+            'headingOptions'    => ['label' => Yii::t('db/category', 'Categories')],
+            'fontAwesome' => true,     
+            'isAdmin'           => true,                       // optional (toggle to enable admin mode)
+            'showFormButtons' => true,
+            'iconEditSettings'=> [
+                'show' => 'list',
+                'type' => TreeView::ICON_CSS,
+                'listData' => [
+                    'folder' => 'Folder',
+                    'file' => 'File',
+                    'mobile' => 'Phone',
+                    'bell' => 'Bell',
+                    'comments' => 'kropki',
+                    'gavel' => 'mlotek',
+                    'briefcase'=>'walizka',
+                    'male' => 'facet',
+                    'user-secret' => 'agent',
+                    'balance-scale' => 'szalka',
+                    'book'=>'książka',
+                    'newspaper-o'=>'gazeta'
+                ]
+            ],
+        //    'wrapperTemplate' =>'
+        //                      {header}
+        //                      {tree}',
+        //  'toolbarOptions'=> ['class'=>'hide'],
+            'displayValue'      => 1,                           // initial display value
+            'softDelete'      => true,                        // normally not needed to change
+            //'cacheSettings'   => ['enableCache' => true]      // normally not needed to change
+        ]);
+    }
 ?>
