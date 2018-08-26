f144e121c595035d6dcb1ab4ef3d3563e938ae0c
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-08-26 18:27:33 +0200

    small fix

--- modules/docmgm/views/document/update.php
+++ modules/docmgm/views/document/update.php
@@ -7,7 +7,7 @@ $this->title = DocmgmModule::t('db/document', 'Update Document: ' . $model->titl
     'nameAttribute' => '' . $model->title,
 ]);
 $this->params['breadcrumbs'][] = ['label' => DocmgmModule::t('db/document', 'Documents'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->doc_id]];
+//$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->doc_id]];
 $this->params['breadcrumbs'][] = DocmgmModule::t('db/document', 'Update');
 ?>
 <div class="document-update">
