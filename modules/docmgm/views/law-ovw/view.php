22d409b3727151802e0bcd2967ed3fe1fd90e4e3
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-09-16 21:42:45 +0200

    doc ovw

--- modules/docmgm/views/law-ovw/view.php
+++ modules/docmgm/views/law-ovw/view.php
@@ -54,59 +54,137 @@ Url::remember();
                     <? endforeach; ?>
                 </ul>
             </div>
-    </div>
+        </div>
     <? endif ?>
+
     <? if (isset($categories[1])) : ?>
+    <? $category = $categories[1]; ?>
     <div class="col-xs-12 col-md-6">
         <div class="row">
             <div class="col-xs-12 col-lg-6">
+                <div class="rights-boxb">
+                    <div class="rights-boxb-head">
+                        <h3 title="<?= $category->name ?>" class="rights-boxb-title">
+                        <?
+                            $catImg = $category->getFilePath();
+                            if (isset($catImg) && $catImg != null)
+                            {
+                                echo  HTML::img($catImg);
+                            }
+                        ?>
+                        <?= $category->name ?>
+                        </h3>
+                        <?=
+                        Html::a(Yii::t('app', 'More') . '<span class="glyphicon glyphicon-menu-right"></span>', 
+                        ['view-all-articles', 'id' => $category->id],
+                        [
+                            'class' => 'hvr-pop btn-red', 
+                            'title' => Yii::t('app', 'More')
+                        ]);
+                        ?>
+                    </div>
+                    <ul class="rights-boxb-lists">
+                        <? foreach($category->documents as $document) : ?>
+                        <li class="item">
+                            <?=
+                                Html::a($document->title, 
+                                ['view-single-article', 'category_id'=> $category->id, 'doc_id' => $document->doc_id],
+                                [
+                                    'class' => 'hvr-pop', 
+                                    'title' => $document->title
+                                ]);
+                            ?>
+                        </li>
+                        <? endforeach; ?>
+                    </ul>
+                </div>
+            </div>
 
-                <div class="rights-boxb">
-                    <div class="rights-boxb-head">
-                        <h3 title="Akty prawne" class="rights-boxb-title"><img src="template/default/images/icn-rights-subtitle-aktualnosci.png" alt=""> Aktualności</h3>
-                        <a href="#" class="hvr-pop btn-red" title="zobacz więcej">więcej <span class="glyphicon glyphicon-menu-right"></span></a>
+            <? if (isset($categories[2])) : ?>
+            <? $category = $categories[2]; ?>
+                <div class="col-xs-12 col-lg-6">
+                    <div class="rights-boxb">
+                        <div class="rights-boxb-head">
+                            <h3 title="<?= $category->name ?>" class="rights-boxb-title">
+                            <?
+                                $catImg = $category->getFilePath();
+                                if (isset($catImg) && $catImg != null)
+                                {
+                                    echo  HTML::img($catImg);
+                                }
+                            ?>
+                            <?= $category->name ?>
+                            </h3>
+                            <?=
+                            Html::a(Yii::t('app', 'More') . '<span class="glyphicon glyphicon-menu-right"></span>', 
+                            ['view-all-articles', 'id' => $category->id],
+                            [
+                                'class' => 'hvr-pop btn-red', 
+                                'title' => Yii::t('app', 'More')
+                            ]);
+                            ?>
+                        </div>
+                        <ul class="rights-boxb-lists">
+                            <? foreach($category->documents as $document) : ?>
+                            <li class="item">
+                                <?=
+                                    Html::a($document->title, 
+                                    ['view-single-article', 'category_id'=> $category->id, 'doc_id' => $document->doc_id],
+                                    [
+                                        'class' => 'hvr-pop', 
+                                        'title' => $document->title
+                                    ]);
+                                ?>
+                            </li>
+                            <? endforeach; ?>
+                        </ul>
                     </div>
-                    <ul class="rights-boxb-lists">
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Lorem ipsum dolor sit amet</a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Ut enim ad minim veniam, quis nostrud exercitation</a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Ullamco laboris nisi ut aliquip ex ea commodo consequat. </a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Duis aute irure dolor in reprehenderit in voluptate.</a></li>
-                    </ul>
                 </div>
-            </div>
-            <div class="col-xs-12 col-lg-6">
-                <div class="rights-boxb">
-                    <div class="rights-boxb-head">
-                        <h3 title="Akty prawne" class="rights-boxb-title"><img src="template/default/images/icn-rights-subtitle-orzeczenia.png" alt=""> Orzeczenia</h3>
-                        <a href="#" class="hvr-pop btn-red" title="zobacz więcej">więcej <span class="glyphicon glyphicon-menu-right"></span></a>
-                    </div>
-                    <ul class="rights-boxb-lists">
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Lorem ipsum dolor sit amet</a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Ut enim ad minim veniam, quis nostrud exercitation</a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Ullamco laboris nisi ut aliquip ex ea commodo consequat. </a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Duis aute irure dolor in reprehenderit in voluptate.</a></li>
-                    </ul>
-                </div>
-            </div>
+            <? endif ?>
         </div>
+
+        <? if (isset($categories[3])) : ?>
+        <? $category = $categories[3]; ?>
         <div class="row">
             <div class="col-xs-12">
                 <div class="rights-boxb">
                     <div class="rights-boxb-head">
-                        <h3 title="Akty prawne" class="rights-boxb-title"><img src="template/default/images/icn-rights-subtitle-komentarze.png" alt=""> Komentarze</h3>
-                        <a href="#" class="hvr-pop btn-red" title="zobacz więcej">więcej <span class="glyphicon glyphicon-menu-right"></span></a>
+                        <h3 title="<?= $category->name ?>" class="rights-boxb-title">
+                            <?
+                                $catImg = $category->getFilePath();
+                                if (isset($catImg) && $catImg != null)
+                                {
+                                    echo  HTML::img($catImg);
+                                }
+                            ?>
+                            <?= $category->name ?>
+                        </h3>
+                        <?=
+                            Html::a(Yii::t('app', 'More') . '<span class="glyphicon glyphicon-menu-right"></span>', 
+                            ['view-all-articles', 'id' => $category->id],
+                            [
+                                'class' => 'hvr-pop btn-red', 
+                                'title' => Yii::t('app', 'More')
+                            ]);
+                        ?>
                     </div>
                     <ul class="rights-boxb-lists">
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Lorem ipsum dolor sit amet</a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Ut enim ad minim veniam, quis nostrud exercitation</a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Ullamco laboris nisi ut aliquip ex ea commodo consequat. </a></li>
-                        <li class="item"><a href="#" class="hvr-pop" title="#">Duis aute irure dolor in reprehenderit in voluptate.</a></li>
+                        <? foreach($category->documents as $document) : ?>
+                            <li class="item">
+                                <?=
+                                    Html::a($document->title, 
+                                    ['view-single-article', 'category_id'=> $category->id, 'doc_id' => $document->doc_id],
+                                    [
+                                        'class' => 'hvr-pop', 
+                                        'title' => $document->title
+                                    ]);
+                                ?>
+                            </li>
+                        <? endforeach; ?>
                     </ul>
                 </div>
             </div>
-    </div>
+        </div>
+        <? endif ?>
     <? endif ?>
 </div>
