3189e650fde63419a6f079105f9c067742ee1c64
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2018-11-22 16:25:37 +0100

    git ignore

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=lamoric_db',
+    'username' => 'app_root',
+    'password' => 'CHFKbdOh6FtEnCgl',
+    'charset' => 'utf8',
+
+ //   'class' => 'yii\db\Connection',
+ //   'dsn' => 'mysql:host=localhost;dbname=lamoric_1',
+ //   'username' => 'app_root_usr',
+ //   'password' => 'Opbd31*5@*()SDW',
+ //   'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
--- models/log/LogQuery.php
+++ models/log/LogQuery.php
@@ -6,14 +6,9 @@ use Yii;
 
 class LogQuery extends \yii\db\ActiveQuery
 {
-    /*public function active()
-    {
-        return $this->andWhere('[[status]]=1');
-    }*/
-
     public function prepare($builder)
     {
-       if (! Yii::$app->user->can('Application Admin')) {  
+       if (! Yii::$app->user->can('Application Admin')) {
             if (isset(Yii::$app->user->identity->profile->company_id) && Yii::$app->user->can('Company Admin') ) {
                 $this->andFilterWhere(['like', 'message', 'COMPANY['.Yii::$app->user->identity->profile->company_id.']']);
             } else {
