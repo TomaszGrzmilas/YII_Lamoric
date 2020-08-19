8d924ba13dd2fd7589c087dec25978f3423d5220
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2020-08-19 23:33:38 +0200

    change config db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=lamoric_db',
-    'username' => 'app_root',
-    'password' => 'CHFKbdOh6FtEnCgl',
+    'dsn' => 'mysql:host=localhost;dbname=lamoric_db',
+    'username' => 'lamoric_admin',
+    'password' => '3e*3ooD4',
     'charset' => 'utf8',
 
  //   'class' => 'yii\db\Connection',
